a75e30fd19f9af8fa09193aaeedcfcff2d095354
tianwei.li <tianwei.li@bitmart.com>
2020-05-03 22:47:17 +0800

    add No53

---
+++ src/algorithm/leetCode/No53/Solution.java
@@ -0,0 +1,18 @@
+package algorithm.leetCode.No53;
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int sum = 0;
+        for(int num: nums) {
+            if(sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+}
+
