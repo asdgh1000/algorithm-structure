6de69aa6a951afdd6a1c6f4599dbaa700d56bedd
tianwei.li <tianwei.li@bitmart.com>
2020-05-08 23:36:39 +0800

    add No221

---
+++ src/algorithm/leetCode/No221/Solution.java
@@ -0,0 +1,45 @@
+package algorithm.leetCode.No221;
+
+/**
+ * 暴力法，遍历矩阵将将每个元素看作正方形左上角 然后遍历判断
+ */
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    // 遇到一个 1 作为正方形的左上角
+                    maxSide = Math.max(maxSide, 1);
+                    // 计算可能的最大正方形边长
+                    int currentMaxSide = Math.min(rows - i, columns - j);
+                    for (int k = 1; k < currentMaxSide; k++) {
+                        // 判断新增的一行一列是否均为 1
+                        boolean flag = true;
+                        if (matrix[i + k][j + k] == '0') {
+                            break;
+                        }
+                        for (int m = 0; m < k; m++) {
+                            if (matrix[i + k][j + m] == '0' || matrix[i + m][j + k] == '0') {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag) {
+                            maxSide = Math.max(maxSide, k + 1);
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        int maxSquare = maxSide * maxSide;
+        return maxSquare;
+    }
+}
+
